89cffc4a3378b5a5a2c57d59b79b69fc92c89f29
catSalem <Galdarkk@gmail.com>
2017-03-14 01:45:53 +0200

    lab_2

--- js/alarm_lab2_js.js
+++ js/alarm_lab2_js.js
@@ -1,9 +1,81 @@
 
-$(".heading_current_time_container").hide();
-$(".heading_current_time_container").fadeIn(5000);
+$(".alarm_wrapping").hide();
+$(".input_chack").hide();
+$(".input_form").hide();
+$(".dialog").hide();
+
+$(".alarm_wrapping").fadeIn(500);
+
+
+
+$(".set_button").click(function() {
+	$( ".input_form" ).fadeIn(500);
+	$(".set_button").hide();
+	$(".reset_button").hide();
+	$(".back_to_main").fadeIn(500);
+
+ 
+;})
+
+
+$(".ass").keyup(function(){
+	$(".accept_alarm").fadeIn(500);
+	$(".decline_alarm").fadeIn(500);
+	$(".back_to_main").hide();
+})
+
+$(".reset_button").click(function(){
+	setTimeout(function() {
+		window.location.reload();}, 5);
+
+})
+
+
+$(".back_to_main").click(function() {
+	$(".input_form").hide();
+	$(".back_to_main").hide();
+	$(".set_button").fadeIn(500);
+	$(".reset_button").fadeIn(500);
+
+})
+
+$(".decline_alarm").click(function(){
+	$(".input_form").hide();
+	$(".set_button").fadeIn(500);
+	$(".reset_button").fadeIn(500);
+	$(".accept_alarm").hide();
+	$(".decline_alarm").hide();
+})
+
 
 setInterval(function() {
-	$(".current_time_s").html(getDate()); } , 1000);
+	$(".current_time_s").html(getDate());
+
+	$(".accept_alarm").click(function() {
+		var alarmii = $(".ass").val();
+
+		if(alarmii) {
+			$(".alarted_text").append(alarmii);				
+    		$(".input_form").html("I will call you at" +" "+ alarmii);
+    		$(".back_to_main").fadeIn(500);
+    		$(".accept_alarm").hide();
+     		$(".decline_alarm").hide();
+        }
+        else{
+        return false;
+        }
+	    });
+	    
+		var alarm_fin = $(".alarted_text").html();
+		if (getDate() == alarm_fin) {
+			alert("Get up, sweaty =_=");
+			setTimeout(function() {window.location.reload();}, 5);
+
+		} 
+
+	}, 500);
+
+
 
 
 
@@ -29,3 +101,5 @@ setInterval(function() {
 
 	return res_current_time = hour +':'+ minute+':'+ second; 
 }
+
+
